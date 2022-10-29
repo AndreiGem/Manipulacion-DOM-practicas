551dac716725efeb2ef73fef54cc2f51bd053420
Andrei T <tandrei511@gmail.com>
2022-10-29 14:53:06 -0500

    primer commit proyecto2

---
+++ main.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1');
+const p1 = document.querySelector('p');
+const p2 = document.querySelector('.p2');
+const pfin = document.querySelector('.pfin');
+
+
+
+h1.setAttribute('class', 'titulo');
+p2.innerText = 'Hola estoy practicando manipulación del DOM en Platzi';
+
